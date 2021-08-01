38335e810c79ee634a2932e9948d0b9528b274d8
Mitchell <mitchell.jones1994@gmail.com>
2021-07-31 18:19:42 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+$(document).ready(function() {
+  $('#publications-btn').click(function() {
+    $('#teaching').fadeOut(function() {
+      $('#publications').fadeIn();
+    });
+  });
+
+  $('#teaching-btn').click(function() {
+    $('#publications').fadeOut(function() {
+      $('#teaching').fadeIn();
+    });
+  })
+});
+*/
